46c5e288026fb7f01ca7383046217eabd56dfe3f
Ryanfsdf <ryanfsdf.kim@gmail.com>
2016-11-06 19:18:16 -0500

    Added empty events

--- event.cpp
+++ event.cpp
@@ -1,3 +1,4 @@
+#include "event.h"
 #include "graphics.h"
 
 void checkEvent() {
@@ -5,11 +6,50 @@ void checkEvent() {
 
 	while (SDL_PollEvent(&event)) {
 		switch (event.type) {
-
 			//Detect window close
-		    case SDL_QUIT:
-		        exit(0);
-		    break;
+			case SDL_QUIT:
+				exit(0);
+			break;
+
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+					case SDLK_UP:
+						//
+					break;
+
+					case SDLK_DOWN:
+						//
+					break;
+
+					case SDLK_LEFT:
+						//
+					break;
+
+					case SDLK_RIGHT:
+						//
+					break;
+
+					case SDLK_SPACE:
+						//
+					break;
+
+					case SDLK_LSHIFT:
+						//
+					break;
+
+					default:
+						//
+					break;
+				}
+			break;
+
+			case SDL_KEYUP:
+				//
+			break;
+
+			default:
+				//
+			break;
 		}
 	}
 }
